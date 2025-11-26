a8b736d5af29b57125a062b954726fb92b77a6b8
FinnjaBayer <g00470359@atu.ie>
2025-11-26 17:31:40 +0000

    added preferred customer class, sub-class from customer

---
+++ src/main/java/ie/atu/PreferredCustomer.java
@@ -0,0 +1,52 @@
+package ie.atu;
+
+public class PreferredCustomer extends Customer{
+    private int loyaltyPoints;
+    private double discountLevel;
+
+    public PreferredCustomer(String name, String address, String phoneNumber, int customerNumber, boolean mailingList, int loyaltyPoints) {
+        super(name, address, phoneNumber, customerNumber, mailingList);
+        this.loyaltyPoints = loyaltyPoints;
+        this.discountLevel = updateDiscountLevel(loyaltyPoints);
+
+    }
+    public PreferredCustomer(){
+        super();
+        this.loyaltyPoints = 0;
+        this.discountLevel = 0;
+    }
+
+    private double updateDiscountLevel(int loyaltyPoints){
+        if(loyaltyPoints <500){
+            this.discountLevel = 0;
+        }
+        else if(loyaltyPoints < 1000){
+            this.discountLevel = 0.05;
+        }
+        else if(loyaltyPoints < 1500){
+            this.discountLevel = 0.06;
+        }
+        else if(loyaltyPoints < 2000){
+            this.discountLevel = 0.07;
+        }
+        else if(loyaltyPoints >= 2000){
+            this.discountLevel = 0.1;
+        }
+        return discountLevel;
+    }
+    public int getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+    public double getDiscountLevel() {
+        return discountLevel;
+    }
+    public void setLoyaltyPoints(int loyaltyPoints) {
+        this.loyaltyPoints = loyaltyPoints;
+        updateDiscountLevel(loyaltyPoints);
+    }
+    public void addLoyaltyPoints(int points) {
+        this.loyaltyPoints += points;
+        updateDiscountLevel(loyaltyPoints);
+    }
+
+}
