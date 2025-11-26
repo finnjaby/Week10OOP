c4a5a8c6c8515011d8efa8d78c43749a7220c59b
FinnjaBayer <g00470359@atu.ie>
2025-11-26 17:48:04 +0000

    added comments, fixed typos

--- src/main/java/ie/atu/Customer.java
+++ src/main/java/ie/atu/Customer.java
@@ -4,31 +4,37 @@ public class Customer extends Person{
     private int customerNumber;
     private boolean mailingList;
 
-    public Customer(String name, String address, String phoneNumber, int customerNumber, boolean mailingList) {
+    public Customer(String name, String address, String phoneNumber, int customerNumber, boolean mailingList) {     //parameterised constructor
         super(name, address, phoneNumber);
         this.customerNumber = customerNumber;
         this.mailingList = mailingList;
 
     }
-    public Customer(){
+    public Customer(){      //default constructor
         super();
         this.customerNumber = 0;
         this.mailingList = false;
     }
+
+    //getters
     public int getCustomerNumber() {
         return customerNumber;
     }
     public boolean getMailingList() {
         return mailingList;
     }
+
+    //setters
     public void setCustomerNumber(int customerNumber) {
         this.customerNumber = customerNumber;
     }
     public void setMailingList(boolean mailingList) {
         this.mailingList = mailingList;
     }
+
+
     @Override
-    public String toString() {
+    public String toString() {      //format output
         return "Customer{" +
                 "person=" + super.toString() +
                 ", customerNumber=" + customerNumber +
